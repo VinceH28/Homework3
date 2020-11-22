3bfe5db7b45dee36c4d441a85d06bd085c2b2894
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2020-11-21 21:30:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,88 +5,57 @@ const generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
   //Alert user to start password creation
-
   const totalLength = prompt(
-
-    "How many characters should your new password be?"
-
-  );
+    "How many characters should your new password be?");
 
   if (totalLength >= 8 && totalLength <= 128) {
 
     alert(`Your new password will be ${totalLength} characters long.`);
 
     // Prompt user for password criteria
-
     let upper = confirm("Would you like to include upper case letters?");
-
     let lower = confirm("Would you like to include lower case letters?");
-
     let symbol = confirm("Would you like to include special characters?");
-
     let number = confirm("Would you like to include numbers?");
-
     let allChars = "";
-
     let password = "";
 
     //Validate user input
-
     if (upper) {
-
       allChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 
     if (lower) {
-
       allChars += "abcdefghijklmnopqrstuvwxyz";
     }
 
     if (symbol) {
-
       allChars += "~`!@#$%^&*()_-+={[}]}|\:;'<,>.?/";
     }
 
     if (number) {
-
       allChars += "0123456789";
     }
 
     //Generate new password that fufills selected criteria
-
     for (i = 0; i < totalLength; i++) {
-
       password += allChars.charAt(Math.floor(Math.random() * allChars.length));
-
     }
-
-    return password;
-
-  } else {
-
+    return password;} 
+  
+    else {
     alert("Your password must be at least 8 to 24 characters. Let's try again.");
-
   }
-
 }
 
-
 // Display new password to the text area:
-
 function generatePassword() {
 
   let newPassword = generatePassword();
-
   let passwordText = document.querySelector("#password");
 
-
-
   passwordText.value = newPassword;
-
 }
 
-
-
 //Add event listener to generate button
-
 generateBtn.addEventListener("click", displayPassword);
